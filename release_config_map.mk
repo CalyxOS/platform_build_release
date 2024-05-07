b03febf708fab6ac4b8dd4e29187747341561ad4
LaMont Jones <lamontjones@google.com>
2024-05-07 10:50:58 -0700

    Remove unsupported release configs
    This drops trunk_food, trunk, and next, none of which are supported
    release configs.
    
    Bug: 328495189
    Test: manual, TH
    Merged-In: Ib02fc4dea3a6d96adb4a0d2b6a732249373844d9
    Merged-In: Ia6b6654c0542394c1a5ceb506a74ac2a9aa5a01d
    Change-Id: Ia6b6654c0542394c1a5ceb506a74ac2a9aa5a01d

--- release_config_map.mk
+++ release_config_map.mk
@@ -20,12 +20,6 @@ local_dir := $(dir $(lastword $(MAKEFILE_LIST)))
 FLAG_DECLARATION_FILES := $(local_dir)build_flags.scl
 
 # Attach the flag value definitions to the various release configurations.
-$(call declare-release-config, next, $(local_dir)build_config/next.scl)
-$(call declare-release-config, trunk, $(local_dir)build_config/trunk.scl)
-$(call declare-release-config, trunk_food, $(local_dir)build_config/trunk_food.scl, trunk)
 $(call declare-release-config, trunk_staging, $(local_dir)build_config/trunk_staging.scl)
-
-# Temporary config to ease testing build flag migration to protobuf
-$(call declare-release-config, protobuf_flags, $(local_dir)build_config/protobuf_flags.scl, trunk_staging)
 
 local_dir :=
