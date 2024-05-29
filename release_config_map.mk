a7bc75ccdeba8b7d57c18716076bfe52eb02e50d
Ian Kasprzak <iankaz@google.com>
2024-05-29 20:36:17 +0000

    Update staging branch aconfig flags and release configs.
    - ap2a and trunk_staging using 2024-06 Release (ab/AP2A.240605.024)
    - Deleting next/trunk_food/trunk build and aconfig flags
    
    
    Ignore-AOSP-First: "AOSP Staging branch"
    Change-Id: Ie697d7eb05cdbc017f6b96259fec72cf9a1ef01a

--- release_config_map.mk
+++ release_config_map.mk
@@ -20,15 +20,9 @@ local_dir := $(dir $(lastword $(MAKEFILE_LIST)))
 FLAG_DECLARATION_FILES := $(local_dir)build_flags.scl
 
 # Attach the flag value definitions to the various release configurations.
-$(call declare-release-config, next, $(local_dir)build_config/next.scl)
-$(call declare-release-config, trunk, $(local_dir)build_config/trunk.scl)
-$(call declare-release-config, trunk_food, $(local_dir)build_config/trunk_food.scl, trunk)
 $(call declare-release-config, trunk_staging, $(local_dir)build_config/trunk_staging.scl)
 
 # 24Q2, aka AP2A
 $(call declare-release-config, ap2a, $(local_dir)build_config/ap2a.scl)
-
-# Temporary, until we remove the old "staging" configs
-$(call declare-release-config, staging, $(local_dir)build_config/trunk_staging.scl)
 
 local_dir :=
